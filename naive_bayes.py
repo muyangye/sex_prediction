e9a19b01a444d03e4f6e326326641f075df80ce5
muyangye <muyangye@usc.edu>
2022-02-26 04:30:39 -0800

    mod comments

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,59 +1,72 @@
 import csv
-totalMale = totalFemale = 0
-charFreqs = {}
 
-# Collect data from the csv traning set
-with open('freqs.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
+# Read data
+def read_data():
+    total_male = total_female = 0
+    char_freqs = {}
+
+    # Collect data from the csv traning set
+    with open("freqs1.csv", encoding="utf-8", newline="") as f:
+        reader = csv.reader(f)
         # Skip the first line
-        if (row[0] == 'char'): continue
-        char = row[0]
-        numMale = int(row[1])
-        numFemale = int(row[2])
-        charFreqs[char] = [numFemale, numMale]
-        totalMale += numMale
-        totalFemale += numFemale
+        next(reader)
+        for row in reader:
+            char = row[0]
+            num_male = int(row[1])
+            num_female = int(row[2])
+            char_freqs[char] = [num_female, num_male]
+            total_male += num_male
+            total_female += num_female
 
-# Collect data from the txt training set
-lines = open('freqs.txt', encoding='utf-8').readlines()[1:]
-for line in lines:
-    line = line.strip().split(',')
-    # Don't need id
-    name = line[1]
-    sex = line[2]
-    if (sex == '1'): totalMale += 1
-    else: totalFemale += 1
-    for char in name:
-        if (char not in charFreqs):
-            charFreqs[char] = [0, 0]
-        charFreqs[char][int(sex)] += 1
+    # Collect data from the txt training set
+    lines = open("freqs.txt", encoding="utf_8").readlines()[1:]
+    for line in lines:
+        line = line.strip().split(",")
+        # Don"t need id
+        name = line[1]
+        sex = line[2]
+        if (sex == "1"): total_male += 1
+        else: total_female += 1
+        for char in name:
+            if (char not in char_freqs):
+                char_freqs[char] = [0, 0]
+            char_freqs[char][int(sex)] += 1
+    
+    return (total_male, total_female, char_freqs)
 
-# charFreqs is essentially charProbabilities now
-# charFreqs[char][0] = P(char|female), charFreqs[char][1] = P(char|male)
-for char in charFreqs:
-    # P(char|sex) = number of occurence of a char in a sex / total number of that sex
-    charFreqs[char][0] /= totalFemale
-    charFreqs[char][1] /= totalMale
 
-while True:
-    print('Please enter the name you want to predict(enter exit to exit): ')
-    name = input()
-    if (name == 'exit'):
-        break
-    # Suppose name = ABC
-    # pTempMale = P(A|male) * P(B|male) * P(C|male), pTempFemale = P(A|female) * P(B|female) * P(C|female)
-    pTempMale = pTempFemale = 1
-    for char in name:
-        # If the user inputs an invalid character
-        if (char not in charFreqs):
-            # Then simply predict without that character
-            print("Sorry! [" + char + "] in the name you provided is not included in the training set! Only predicting based on rest of the characters!")
-            continue
-        pTempMale *= charFreqs[char][1]
-        pTempFemale *= charFreqs[char][0]
-    pMale = pTempMale * totalMale / (pTempMale * totalMale + pTempFemale * totalFemale)
-    pFemale = pTempFemale * totalFemale / (pTempMale * totalMale + pTempFemale * totalFemale)
-    print("Probability of Male: " + str(pMale*100) + '%')
-    print("Probability of Female: " + str(pFemale*100) + '%')
-    print('\n')
+# Naive Bayes logic
+def main():
+    total_male, total_female, char_freqs = read_data()
+    # char_freqs is essentially char_probabilities now
+    # char_freqs[char][0] = P(char|female), char_freqs[char][1] = P(char|male)
+    for char in char_freqs:
+        # P(char|sex) = number of occurence of a char in a sex / total number of that sex
+        char_freqs[char][0] /= total_female
+        char_freqs[char][1] /= total_male
+
+    while True:
+        print("Please enter the name you want to predict(enter exit to exit): ")
+        name = input()
+        if (name == "exit"):
+            break
+        # Suppose name = ABC
+        # p_temp_male = P(A|male) * P(B|male) * P(C|male), p_temp_female = P(A|female) * P(B|female) * P(C|female)
+        p_temp_male = p_temp_female = 1
+        for char in name:
+            # If the user inputs an invalid character
+            if (char not in char_freqs):
+                # Then simply predict without that character
+                print("Sorry! [" + char + "] in the name you provided is not included in the training set! Only predicting based on rest of the characters!")
+                continue
+            p_temp_male *= char_freqs[char][1]
+            p_temp_female *= char_freqs[char][0]
+        p_male = p_temp_male * total_male / (p_temp_male * total_male + p_temp_female * total_female)
+        p_female = p_temp_female * total_female / (p_temp_male * total_male + p_temp_female * total_female)
+        print("Probability of Male: " + str(p_male*100) + "%")
+        print("Probability of Female: " + str(p_female*100) + "%")
+        print("\n")
+
+
+if __name__ == "__main__":
+    main()
